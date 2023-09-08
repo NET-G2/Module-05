2d13e63fcca84b4e56be6c1b9c68e611003830cd
Miraziz.Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2023-09-08 22:36:27 +0500

    Initial commit

---
+++ Lesson01/Supermarket/CategoryDbService.cs
@@ -0,0 +1,15 @@
+﻿namespace Supermarket
+{
+    internal class CategoryDbService
+    {
+        // CRUD operations
+        // CRUD operations
+        // Create
+        // ReadAll
+        // ReadById
+        // Update
+        // Delete
+        // ReadByName
+        // ReadByCountProducts more than param (ex. 5)
+    }
+}
---
+++ Lesson01/Supermarket/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductService db = new ProductService();
+
+            db.ReadAllProducts();
+            Console.WriteLine();
+
+            db.CreateProduct("CocaCola", 25000);
+            Console.WriteLine();
+
+            db.ReadAllProducts();
+            Console.WriteLine();
+
+            db.UpdateProduct(1, "Water", 50000);
+            Console.WriteLine();
+
+            db.ReadAllProducts();
+            Console.WriteLine();
+
+            db.DeleteProduct(6);
+            db.ReadAllProducts();
+
+            Console.ReadKey();
+        }
+    }
+}
