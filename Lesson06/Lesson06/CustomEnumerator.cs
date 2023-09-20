101cbc77ff38f539b1afbd1f5956f508669944a9
Abror <nabiddinovabror8@gmail.com>
2023-09-20 13:05:35 +0500

    Code clear

---
+++ Lesson06/Lesson06/CustomEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson06
+{
+    public class CustomEnumerator : IEnumerator
+    {
+        private readonly string[] _items;
+        private readonly int _count;
+        private int _currentIndex;
+
+        public CustomEnumerator(string[] items)
+        {
+            _items = items;
+            _count = items.Length;
+            _currentIndex = -1;
+        }
+
+        public object Current => _items[_currentIndex];
+
+        public bool MoveNext()
+        {
+            if (_currentIndex < _count - 1)
+            {
+                _currentIndex++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            _currentIndex = -1;
+        }
+    }
+}
